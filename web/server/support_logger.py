1a4ccd316c4649e0ed4352705fecf34b1b299c83
rajgu764-ui <rajgu764@gmail.com>
2026-04-22 03:15:05 +0600

    Update support_logger.py

--- web/server/support_logger.py
+++ web/server/support_logger.py
@@ -2,7 +2,7 @@ import logging
 import sys
 import traceback
 
-SUPPORT = "https://t.me/AV_SUPPORT_GROUP"
+SUPPORT = "https://t.me/I_LinkerChats"
 
 # ANSI Colors for Terminal Highlighting
 class Colors:

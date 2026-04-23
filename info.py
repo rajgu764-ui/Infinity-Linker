365becf7e92e5cee824803df2cced615e496860c
rajgu764-ui <rajgu764@gmail.com>
2026-04-23 09:09:54 +0600

    Update info.py

--- info.py
+++ info.py
@@ -62,11 +62,11 @@ IS_SHORTLINK = is_enabled(environ.get('IS_SHORTLINK', "True"), True)
 
 # Verification Config
 VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 60)) # In Minutes/Hours based on logic
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://cuty.io/api')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'cuty.io')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'fe5e689e49f014fbfc6978495')
 
 # Second Verification Config
-SHORTLINK_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "https://cuty.io/api")
+SHORTLINK_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "cuty.io")
 SHORTLINK_API2 = environ.get("SHORTENER_API2", "https://cuty.io/api")
 
 # =========================================================

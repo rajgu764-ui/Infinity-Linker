b387a3f9ea2807499f3557249fcb458904dd7ec1
rajgu764-ui <rajgu764@gmail.com>
2026-04-29 02:49:39 +0600

    Update check_verification.py

--- plugins/check_verification.py
+++ plugins/check_verification.py
@@ -57,11 +57,15 @@ async def av_x_verification(client, message):
         is_second_shortener
     )
 
-    buttons = [[
+    buttons = [
+    [
         InlineKeyboardButton(text="✅ ᴠᴇʀɪꜰʏ ✅", url=verify_url), 
-        InlineKeyboardButton(text="⁉️ ʜᴏᴡ ᴛᴏ ᴠᴇʀɪꜰʏ ⁉️", url=how_to_download_link),
+        InlineKeyboardButton(text="⁉️ ʜᴏᴡ ᴛᴏ ᴠᴇʀɪꜰʏ ⁉️", url=how_to_download_link)
+    ],
+    [
         InlineKeyboardButton(text="😁 ʙᴜʏ ꜱᴜʙꜱᴄʀɪᴘᴛɪᴏɴ - ɴᴏ ɴᴇᴇᴅ ᴛᴏ ᴠᴇʀɪꜰʏ 😁", callback_data="premium_info")
-    ]]
+    ]
+    ]
 
     user_name = message.from_user.first_name
 
